cab20bac09e525d0566a20e6dcf57b5fcecab98b
Jesus Christian Cruz Acono <compermisos@gmail.com>
2011-03-30 01:32:09 -0600

    added log logic

--- db_manager.php
+++ db_manager.php
@@ -1,15 +1,20 @@
 <?php
 function connect() {
+	global $logger;
 	$databaseResponse = mysql_connect(MYSQL_SERVER, MYSQL_USER, MYSQL_PASSWD);
-	$selectResult = mysql_select_db( MYSQL_DB ) or die();
+	$logger->debug('start conection to db');
+	$selectResult = mysql_select_db( MYSQL_DB ) or die($logger->fatal('the conection fail'));
 }
 
 function executeQuery($query) {
+	global $logger;
+	$logger->info('start execution to db');
 	connect();
+	$logger->debug('query to execute ' . $query);
 	$result = mysql_query($query);
 	$err = mysql_error();
 	if( $err != "" ) {
-		echo "Error: $err";
+		$logger->fatal('the query fail. MySQL error: ' . $err);
 		exit();
 	} else {
 		mysql_close();
